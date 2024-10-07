075760b56f861622bb5760b8335a92ae0e0a4c02
GlenALarson <markkula.antti@gmail.com>
2024-10-07 15:19:40 +0300

    app is working

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, } from 'react-native';
+import React, { useState, useEffect } from 'react';
+
+// App gets daily cat facts from the API and displays them.
+
+const URL = 'https://cat-fact.herokuapp.com/facts'
 
 export default function App() {
+  
+  const [facts, setFacts] = useState([])
+
+  useEffect(() => {
+    fetch(URL)
+      .then(response => response.json())
+      .then((json) => {
+        var data = []
+        for (let i = 0; i <= 4; i++) {
+          data.push(json[i].text)
+        }
+        setFacts(data)
+      }).catch((error) => {
+        console.log(error)
+      })
+  }, [])
+  
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Random cat facts</Text>
+      <Text style={styles.text}>{facts[0]}</Text>
+      <Text style={styles.text}>{facts[1]}</Text>
+      <Text style={styles.text}>{facts[2]}</Text>
+      <Text style={styles.text}>{facts[3]}</Text>
+      <Text style={styles.text}>{facts[4]}</Text>
     </View>
   );
 }
@@ -17,4 +44,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    marginBottom: 10,
+  }
 });
